8ec77c921d90622c39c3364826e0544bcc2e9d54
Sanchit Sharma <ssharmas10662@gmail.com>
2025-06-28 12:38:27 +0530

    database: Get url from settings.

--- api/src/db/database.py
+++ api/src/db/database.py
@@ -1,11 +1,12 @@
-import os
 from collections.abc import Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+from src.core.config import settings
+
+DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
