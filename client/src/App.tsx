0a3074a6138f243c78fb4b76748fab7c392bf297
Sanchit Sharma <ssharmas10662@gmail.com>
2026-02-21 15:24:09 +0530

    package.json: Add new dependency.
    Using react-hot-toast for error and success notifications.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,23 @@
+import { toast, Toaster, ToastBar } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./app/router/appRouter";
+
 import "./App.css";
 
 const App = () => (
   <BrowserRouter>
     <div className="App">
       <AppRouter />
+      <Toaster reverseOrder>
+        {(t) => (
+          <div
+            className="w-auto cursor-pointer"
+            onClick={() => toast.dismiss(t.id)}
+          >
+            <ToastBar toast={t} />
+          </div>
+        )}
+      </Toaster>
     </div>
   </BrowserRouter>
 );
